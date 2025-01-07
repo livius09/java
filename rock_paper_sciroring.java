fb006ef255fbd15d961a2a9bbd4ddf8f88f2dc7b
livius09 <levi.wimroither@hotmail.com>
2025-01-07 08:28:05 +0100

    Update rock_paper_sciroring.java

--- rock_paper_sciroring.java
+++ rock_paper_sciroring.java
@@ -5,30 +5,51 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         Random rand=new Random();
-
-        System.out.println("----------");
-        System.out.println("Schere Stein Papier");
-        System.out.println("----------");
-        byte user;
-
         do {
-            System.out.print("wähle schere(1), stein(2) oder Papier(3): ");
-            user= sc.nextByte();
-        }while (!(user<3 && user>0));
-        int computer=rand.nextInt(3)+1;
-        System.out.println("computer hat "+computer+" gewählt");
+            System.out.println("----------");
+            System.out.println("Schere Stein Papier");
+            System.out.println("----------");
+            byte user;
 
-        if (user==computer){
-            System.out.println("patt keiner hat gewonen");
+            do {
+                System.out.print("wähle schere(1), stein(2) oder Papier(3): ");
+                user= sc.nextByte();
+            }while (!(user<=3 && user>0));
+            int computer=rand.nextInt(3)+1;
+            System.out.println("computer hat "+computer+" gewählt");
 
-        }else if (user==1&&computer==2){
-            System.out.println("computer hat gewonnen");
-
-        }else if (user==2&&computer==1){
-            System.out.println("user hat gewonnen");
-        }
+            if (user==computer){
+                System.out.println("patt keiner hat gewonen");
 
 
+            }else if (user==1&&computer==2){
+                System.out.println("computer hat gewonnen");
+
+            }else if (user==2&&computer==1){
+                System.out.println("user hat gewonnen");
+
+
+            }else if (user==2&&computer==3){
+                System.out.println("computer hat gewonnen");
+
+            }else if (user==3&&computer==2){
+                System.out.println("user hat gewonnen");
+
+
+            }else if (user==1&&computer==3){
+                System.out.println("computer hat gewonnen");
+
+            }else if (user==3&&computer==1){
+                System.out.println("user hat gewonnen");
+
+
+            }else{
+                System.out.println("fehler: "+computer);
+            }
+
+            System.out.println("----------");
+            System.out.println("wilst du aufhören ? ja(1), Nein(0)");
+        }while (sc.nextByte()==0);
 
     }
 }
