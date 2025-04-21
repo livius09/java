27945d4585fbef20a8f6e2131059cc706b531b60
livius09 <levi.wimroither@hotmail.com>
2025-04-21 18:33:07 +0200

    Update calc.java

--- calc.java
+++ calc.java
@@ -8,19 +8,40 @@ public class Main {
         double b;
         Scanner sc=new Scanner(System.in);
         do {
-            System.out.println("was Wilst du tun: \n1.Adiren \n2.Subtrahieren\n3.Multiplizieren\n4.Dividiren\n5.Quadrieren\n6.Wurzel Zihen");
+            System.out.println("was Wilst du tun: \n1.Adiren \n2.Subtrahieren\n3.Multiplizieren\n4.Dividiren\n5.Quadrieren\n6.Wurzel Ziehen");
             chose= sc.nextInt();
-        }while (chose>0 && chose<7);
+        }while (chose<0 || chose>7);
         System.out.println("zahl eingeben");
         a=sc.nextDouble();
         System.out.println("Zahl Zwei eingeben");
         b=sc.nextDouble();
+
+
+        switch (chose){
+            case 1:
+                System.out.println(addiren(a,b));
+
+                break;
+            case 2:
+                System.out.println(subtrahieren(a,b));
+                break;
+            case 3:
+                System.out.println(multiply(a,b));
+                break;
+            case 4:
+                System.out.println(divide(a,b));
+                break;
+            case 5:
+                System.out.println(sqr(a));
+            case 6:
+                System.out.println(root(a));
+        }
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     public static double addiren(double a,double b){
         return a+b;
     }
@@ -30,7 +51,7 @@ public class Main {
     public static double multiply(double a,double b){
         return a*b;
     }
-    public static double Divide(double a,double b){
+    public static double divide(double a,double b){
         return a/b;
     }
     public static double root(double a){
